ec90d17c0ea4c5aabeef7a4a13db06b0b96286af
Daniela Danailova <d.k.danailova@gmail.com>
2018-11-26 20:44:25 +0200

    Minor changes plus gender toggle modifications

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,27 +1,26 @@
 function toggleGender() {
-        // let boy = document.getElementsByClassName('boy')[0];
-        // let girl = document.getElementsByClassName('girl')[0];
+        let boy = document.querySelector('.boy');
+        let girl = document.querySelector('.girl');
 
-        // let boyCheckBox = document.getElementById('gender-main-toggle-boy');       
-        // let girlCheckBox = document.getElementById('gender-main-toggle-girl');
+
+        let boyCheckBox = document.querySelectorAll('input[type="checkbox"]')[0];
+        let girlCheckBox = document.querySelectorAll('input[type="checkbox"]')[1];
         
-        // girl.addEventListener('click', function(){
-        //         boyCheckBox.removeAttribute('checked');
-        // });
+        girl.addEventListener('click', function(){
+                boyCheckBox.removeAttribute('checked');
+        });
         
-        // boy.addEventListener('click', function(){
-        //         girlCheckBox.removeAttribute('checked');
-        // });
+        boy.addEventListener('click', function(){
+                girlCheckBox.removeAttribute('checked');
+        });
 
+//    $('.gender-toggle.boy').on('click', function(ev){
+//             $('#gender-main-toggle-girl').removeAttr('checked');
+//             $('#gender-main-toggle-girl').prop('checked', false);
+//    });
 
-
-   $('.gender-toggle.boy').on('click', function(ev){
-            $('#gender-main-toggle-girl').removeAttr('checked');
-            $('#gender-main-toggle-girl').prop('checked', false);
-   });
-
-   $('.gender-toggle.girl').on('click', function(ev){
-        $('#gender-main-toggle-boy').removeAttr('checked');
-        $('#gender-main-toggle-boy').prop('checked', false);
-});
+//    $('.gender-toggle.girl').on('click', function(ev){
+//         $('#gender-main-toggle-boy').removeAttr('checked');
+//         $('#gender-main-toggle-boy').prop('checked', false);
+// });
 }
